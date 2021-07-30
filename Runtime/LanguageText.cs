6044a537b2cf34c28fc01cb6392f7a4a1ebf9e9d
Karsion <karsion@126.com>
2021-07-30 14:46:19 +0800

    Update LanguageText.cs
    增加Inspector的属性重绘

--- Runtime/LanguageText.cs
+++ Runtime/LanguageText.cs
@@ -4,6 +4,67 @@ using UnityEngine;
 using UnityEngine.UI;
 using Object = UnityEngine.Object;
 
+
+#region UNITY_EDITOR
+
+#if UNITY_EDITOR
+
+[CustomPropertyDrawer(typeof(LanguageText.TextData))]
+public class TextTextDataDrawer : PropertyDrawer
+{
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+	{
+        return 20 * 4;
+    }
+
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent title)
+	{
+		static void NextLine(ref Rect rect, ref Rect lineToggle1)
+		{
+			rect.y += EditorGUIUtility.singleLineHeight + 2;
+			lineToggle1.y += EditorGUIUtility.singleLineHeight + 2;
+		}
+
+		LanguageText ltmp = property.serializedObject.targetObject as LanguageText;
+		GUIContent empty = EditorGUIUtility.TrTextContent(string.Empty);
+		Rect line = position;
+		line.height = EditorGUIUtility.singleLineHeight;
+		Rect lineToggle = line;
+		lineToggle.width = 20;
+		line.x += lineToggle.width;
+		line.width -= lineToggle.width;
+		ltmp.isOverrideFontAsset = EditorGUI.ToggleLeft(lineToggle, empty, ltmp.isOverrideFontAsset);
+		using (new EditorGUI.DisabledScope(!ltmp.isOverrideFontAsset))
+		{
+			EditorGUI.PropertyField(line, property.FindPropertyRelative("fontMaterial"), true);
+		}
+
+		NextLine(ref line, ref lineToggle);
+		ltmp.isOverrideFontSize = EditorGUI.ToggleLeft(lineToggle, empty, ltmp.isOverrideFontSize);
+		using (new EditorGUI.DisabledScope(!ltmp.isOverrideFontSize))
+		{
+			EditorGUI.PropertyField(line, property.FindPropertyRelative("fontSize"), true);
+		}
+
+		NextLine(ref line, ref lineToggle);
+		ltmp.isOverrideLineSpacing = EditorGUI.ToggleLeft(lineToggle, empty, ltmp.isOverrideLineSpacing);
+		using (new EditorGUI.DisabledScope(!ltmp.isOverrideLineSpacing))
+		{
+			EditorGUI.PropertyField(line, property.FindPropertyRelative("lineSpacing"), true);
+		}
+
+		NextLine(ref line, ref lineToggle);
+		ltmp.isOverrideScale = EditorGUI.ToggleLeft(lineToggle, empty, ltmp.isOverrideScale);
+		using (new EditorGUI.DisabledScope(!ltmp.isOverrideScale))
+		{
+			EditorGUI.PropertyField(line, property.FindPropertyRelative("scale"), true);
+		}
+	}
+}
+#endif
+
+#endregion
+
 [RequireComponent(typeof(Text))]
 public class LanguageText : MonoBehaviourLanguage
 {
